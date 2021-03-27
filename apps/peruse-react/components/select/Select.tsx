9c9b2bfc86071876e8234e4d1fa976fbe61cafe5
Omair Nabiel <nabiel.omair@gmail.com>
2021-03-28 03:03:50 +0500

    add custom select with clickOutside close

--- apps/peruse-react/components/select/Select.tsx
+++ apps/peruse-react/components/select/Select.tsx
@@ -1,31 +1,66 @@
-import React, {useState} from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { Button } from "../index"
 import styles from "./Select.module.scss"
 
+
+interface Option {
+    label: string;
+    value: string | null;
+}
 interface Props {
     label: string;
-    options: Array<string>;
+    options: Option[];
+    handleChange: Function
     isMulti?: boolean;
     isSearchable?: boolean;
 }
-export const Select = ({ label, options, isMulti, isSearchable }: Props) => {
-    let [isOpen, setIsOpen] = useState(false)
 
-    const onClickHandler = () => {
+
+
+export const Select = ({ label, options, handleChange, isMulti, isSearchable }: Props) => {
+    let [isOpen, setIsOpen] = useState(false)
+    let [selected, setSelected] = useState("")
+    let selectRef = useRef(null)
+
+    const selectClickHandler = () => {
         setIsOpen(!isOpen)
     }
 
+    const optionsClickHandler = (option: Option) => {
+        setIsOpen(false)
+        setSelected(option.label)
+        handleChange(option.value)
+    }
+
+    const mousedownEventHandler = (event: MouseEvent) => {
+        const clickedElement = (selectRef.current as any)
+        if (!clickedElement?.contains(event.target)) setIsOpen(false)
+    }
+
+    useEffect(() => {
+        console.log("Options", options)
+        document.addEventListener("mousedown", mousedownEventHandler)
+
+        return () => document.removeEventListener("mousedown", mousedownEventHandler)
+    },[])
+
+
     return (
-        <div className={styles.container}>
-            <Button title={label} onClick={onClickHandler}>
+        <div ref={selectRef} className={styles.container}>
+            <Button title={selected || label} onClick={selectClickHandler}>
                 <div className={`${styles['menu-button-icon']} fas fa-chevron-down`}></div>
 
             </Button>
-
-            <div className={`${styles['menu']}`} style={{display: isOpen? 'block' : 'none' }}>
-                    {options && options.map(option => <div className={`${styles['menu-item']}`}>{option}</div>)}
+            <div className={`${styles['menu']}`} style={{ display: isOpen ? 'block' : 'none' }}>
+                {
+                    options &&
+                    options.map(option => (
+                        <div className={`${styles['menu-item']}`} onClick={() => optionsClickHandler(option)}>
+                            {option.label}
+                        </div>)
+                    )
+                }
             </div>
         </div>
-
     )
 }
--- apps/peruse-react/widgets/toolbar/Toolbar.tsx
+++ apps/peruse-react/widgets/toolbar/Toolbar.tsx
@@ -6,9 +6,24 @@ import { Button, Select } from "../../components"
 
 export const Toolbar = () => {
 
+    const options = [
+        {
+            label: "Books",
+            value: "books"
+        },
+        {
+            label: "Highlights",
+            value: "highlights"
+        }
+    ]
+
+    const bookSelectHandleChange = (selectedOption: string) => {
+        console.log("Selected Option", selectedOption)
+    }
+    
     return (
         <div className="is-flex is-justify-content-space-between">
-            <Select label="Books" options={["Books", "Highlights"]}/>
+            <Select label="Books" options={options} handleChange={bookSelectHandleChange}/>
             <Button title="Upload" onClick={()=>{}}>
             <div className={`${styles['button-icon']} fas fa-arrow-up`}></div>
             </Button>
