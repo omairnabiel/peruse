e3af7c5472cad6840dd237d8527205d2fe9dfdfc
Omair Nabiel <nabiel.omair@gmail.com>
2021-05-04 11:52:54 -0700

    Set prettier on IDE and apply formatting

--- apps/peruse-react/components/icon-button/IconButton.tsx
+++ apps/peruse-react/components/icon-button/IconButton.tsx
@@ -1,32 +1,41 @@
-import React from "react"
+import React from "react";
 
-import style from "./IconButton.module.scss"
+import style from "./IconButton.module.scss";
 
 interface Props {
-     name: string;
-     size: number;
-     title?: string
-     className?: string;
-     onClickHandler: React.MouseEventHandler<HTMLElement>
+    name: string;
+    size: number;
+    title?: string;
+    className?: string;
+    onClickHandler: React.MouseEventHandler<HTMLElement>;
 }
 
 /**
- * `IconButton` uses material icons. For icon names refer to the following link 
- * 
+ * `IconButton` uses material icons. For icon names refer to the following link
+ *
  * @link {https://fonts.google.com/icons}
  * @param {string} name
  * @param {string} title
  * @param {number} size
  * @param {React.MouseEventHandler<HTMLElement>} onClickHandler
- * @returns  {JSX.Element} 
+ * @returns  {JSX.Element}
  */
-export const IconButton = ({name, title, size, onClickHandler}:Props) => {
+export const IconButton = ({ name, title, size, onClickHandler }: Props) => {
     return (
-        <span style={{cursor: 'pointer'}} className="icon-text" onClick={onClickHandler}>
+        <span
+            style={{ cursor: "pointer" }}
+            className="icon-text"
+            onClick={onClickHandler}
+        >
             <span className={`icon ${style[`scale-hover`]}`}>
-                <i className="material-icons-outlined" style={{fontSize: `${size}rem`}}>{name}</i>
+                <i
+                    className="material-icons-outlined"
+                    style={{ fontSize: `${size}rem` }}
+                >
+                    {name}
+                </i>
             </span>
             {title && <span>{title}</span>}
         </span>
-    )
-}
+    );
+};
