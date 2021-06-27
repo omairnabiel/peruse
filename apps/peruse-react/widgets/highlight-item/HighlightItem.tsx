6be970bfaadd39579d96486f6408d61f401d7e01
Omair Nabiel <nabiel.omair@gmail.com>
2021-06-27 16:01:17 +0500

    fix: small styling fixes

--- apps/peruse-react/widgets/highlight-item/HighlightItem.tsx
+++ apps/peruse-react/widgets/highlight-item/HighlightItem.tsx
@@ -1,4 +1,4 @@
-import {Icon, IconButton, Spacer} from '../../components'
+import {Icon, IconButton} from '../../components'
 
 import styles from './HighlightItem.module.scss'
 
@@ -12,40 +12,31 @@ interface Props {
 	location: string
 }
 
-export const HighlightItem = ({color, text, note, cover, name, date, location}: React.PropsWithChildren<Props>) => {
+export const HighlightItem = ({color, text, note, date, location}: React.PropsWithChildren<Props>) => {
 	return (
 		<div className={styles.container} style={{borderLeft: `6px solid ${color}`}}>
-			{/* <div>
-				<img src={cover} className={styles['image']} style={{height: 42, width: 32}} />
-			</div> */}
-
-			<div className={styles['highlight-content']}>
-				{/* Highlight Title */}
-				<div className={styles['title-bar']}>
-					<div className="flex">
-						<div className={styles['book-info']}>
-							{/* <p className={styles['book-title']}>{name}</p> */}
-							<p className={styles['highlight-info']}>
-								{location} - {date}
-							</p>
-						</div>
-					</div>
+			<div className="flex-1 pl-3 mt-1">
+				<div className="flex">
+					<p className="text-xs italic">
+						{location} | {date}
+					</p>
 
 					{/* Highlight Actions */}
-					<div className={`flex flex-1 items-end ${styles.actions}`}>
-						<IconButton name="favorite_border" size={1.5} onClickHandler={() => {}} />
-						<Spacer marginLeft="1" marginRight="1" />
-						<IconButton name="delete" size={1.5} onClickHandler={() => {}} />
+					<div className="flex flex-1 justify-end">
+						<IconButton name="star_outline" size={1.4} onClickHandler={() => {}} />
+						<div className="mx-2 text-red-400">
+							<IconButton name="delete" size={1.4} onClickHandler={() => {}} className="" />
+						</div>
 					</div>
 				</div>
 
-				{/* Highlight Content */}
-				<div className={styles['highlight-text']}>{text}</div>
+				{/* Highlight Text and Note */}
+				<div className="font-sf-pro text-lg my-1">{text}</div>
 				{note && (
-					<div className={styles['highlight-note']}>
+					<div className="flex pb-2 items-center font-sf-pro text-sm italic text-gray-500 ">
 						<Icon name="description" size={1.1} type="outlined" />
 
-						<div>{note}</div>
+						<div className="mx-2">{note}</div>
 					</div>
 				)}
 			</div>
